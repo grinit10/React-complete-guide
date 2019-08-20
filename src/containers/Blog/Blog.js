e5e751d2eee043ec8d6e592eca06640d7f14b0e2
Arpita <appypro90@gmail.com>
2019-08-20 12:53:16 +1000

    Added Routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,61 +1,25 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from '../../Axios';
+import Posts from '../Blog/Posts/Posts';
+import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
-    state = {
-        posts : [],
-        selectedPost : null,
-        error : false
-    }
-    componentDidMount(){
-        axios.get('/posts')
-                .then(response => {
-                    console.log("frpm local request");
-                    const posts = response.data.slice(0, 4);
-                    const updatedPost = posts.map(post => {
-                        return {
-                            ...post,//it will map Array's properties.
-                            author : 'Arpita'
-                        }
-                    })
-                    this.setState({posts : updatedPost});
-                })
-                .catch(error => {
-                    console.log("from local error");
-                    this.setState({error : true});
-                });
-    }
-
-    fullPostHandler = (id) => {
-        this.setState({selectedPost : id});
-    }
     render () {
-        let posts = <p>Something went wrong!</p>
-        if(!this.state.error){
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    click={() => this.fullPostHandler(post.id)} />;
-            })
-        }
-        
         return (
             <div>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <nav className='Blog'>
+                    <ul>
+                        <li><a href='/home'>Home</a></li>
+                        <li><a href='/full-post'>Full Post</a></li>
+                    </ul>
+                </nav>
                 <section>
-                    <FullPost id={this.state.selectedPost}/>
-                </section>
-                <section>
-                    <NewPost />
+                    {/* <Route path='/' render={() => <Posts />}/> */}
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' component={NewPost} />
+
                 </section>
             </div>
         );
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from '../../../Axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+
+class Posts extends Component {
+	state = {
+        posts : [],
+        selectedPost : null,
+        error : false
+	}
+	
+	componentDidMount(){
+        axios.get('/posts')
+                .then(response => {
+                    console.log("from local request");
+                    const posts = response.data.slice(0, 4);
+                    const updatedPost = posts.map(post => {
+                        return {
+                            ...post,//it will map Array's properties.
+                            author : 'Arpita'
+                        }
+                    })
+                    this.setState({posts : updatedPost});
+                })
+                .catch(error => {
+                    console.log("from local error");
+                    this.setState({error : true});
+                });
+	}
+	
+	fullPostHandler = (id) => {
+        this.setState({selectedPost : id});
+	}
+	
+	render(){
+		let posts = <p>Something went wrong!</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    click={() => this.fullPostHandler(post.id)} />;
+            })
+        }
+		return(
+			<section className="Posts">
+                    {posts}
+            </section>
+		);
+	}
+}
+
+export default Posts;
